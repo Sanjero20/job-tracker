dc35c4a085486ad885f9a359a3e7bf41134ec08d
Sanjero20 <gelosantos72@gmail.com>
2024-05-31 21:58:23 +0800

    add modal for adding applications

---
+++ src/components/modals/add-application.tsx
@@ -0,0 +1,28 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "../ui/button";
+import ApplicationForm from "../form";
+
+function AddApplcation() {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Button>Add Application</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add new application</DialogTitle>
+        </DialogHeader>
+
+        <ApplicationForm />
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default AddApplcation;
