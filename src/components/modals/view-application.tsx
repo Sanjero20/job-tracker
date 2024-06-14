f64f6a59521f51dd1ea9a0f57ae321a679da011b
Sanjero20 <gelosantos72@gmail.com>
2024-06-14 14:59:46 +0800

    add content to view modal

--- src/components/modals/view-application.tsx
+++ src/components/modals/view-application.tsx
@@ -1,12 +1,8 @@
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-} from "../ui/dialog";
+import { Dialog, DialogContent, DialogFooter } from "@/components/ui/dialog";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 import { useViewModal } from "@/stores/viewModalStore";
+import { Button } from "../ui/button";
 
 function ViewApplicationModal() {
   const { data, isOpen, toggleModal } = useViewModal();
@@ -15,15 +11,38 @@ function ViewApplicationModal() {
     <Dialog open={isOpen} onOpenChange={toggleModal}>
       {data && (
         <DialogContent>
-          <DialogHeader>
-            <DialogTitle className="text-center font-bold">
-              {data.company_name}
-            </DialogTitle>
-          </DialogHeader>
+          <section className="flex items-end justify-between">
+            <div className="flex flex-col">
+              <h2 className="font-bold">{data.position}</h2>
+              <h3>
+                {data.company_name} &middot; {data.setup}
+              </h3>
+            </div>
 
-          <p>{data.status}</p>
+            <p>{data.status}</p>
+          </section>
 
-          <DialogFooter></DialogFooter>
+          {/* Salary */}
+          <section>
+            <p>
+              Salary: {data.min_compensation} - {data.max_compensation}
+            </p>
+            <p>Applied on {data.application_date}</p>
+          </section>
+
+          {/* Note */}
+          {data.note && (
+            <section className="">
+              <p>Note</p>
+              <ScrollArea className="h-24 whitespace-pre-line rounded border border-primary p-2 text-sm">
+                {data.note}
+              </ScrollArea>
+            </section>
+          )}
+
+          <DialogFooter>
+            <Button className="w-full">View Job Posting</Button>
+          </DialogFooter>
         </DialogContent>
       )}
     </Dialog>
