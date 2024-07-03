e7d23756328b22d1c35de322c1a168b1f725c897
Sanjero20 <gelosantos72@gmail.com>
2024-07-03 14:50:14 +0800

    feat: add link to navigate to accounts page

--- src/layouts/sidebar.tsx
+++ src/layouts/sidebar.tsx
@@ -1,18 +1,27 @@
 import Navbar from "./navbar";
 import LogoutButton from "@/components/logout-btn";
 import { Card } from "@/components/ui/card";
+import { SquareUserRound } from "lucide-react";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
   return (
     <aside className={"min-w-[250px] max-w-[250px] flex-1"}>
       <Card className="flex h-full w-full flex-col justify-between p-4">
         {/* Upper part */}
-        <section className="flex flex-col gap-8">
+        <section className="flex flex-col gap-2">
           {/* Account */}
           <div className="flex items-center gap-4">
-            <div className="h-8 w-8 rounded-full bg-neutral-400"></div>
-            <p>Account Name</p>
+            <Link
+              to={"/account"}
+              className="flex w-full gap-4 rounded p-2 hover:bg-neutral-200"
+            >
+              <SquareUserRound />
+              <p>Account Name</p>
+            </Link>
           </div>
+
+          <hr></hr>
 
           <Navbar />
         </section>
