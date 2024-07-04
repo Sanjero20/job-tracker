754ca28247e51dd5db3904d5fe3ce270b28e0b6d
Sanjero20 <gelosantos72@gmail.com>
2024-07-04 16:42:30 +0800

    feat: initialize account page content

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,5 +1,29 @@
+import { useAccount } from "@/stores/account";
+
 function AccountPage() {
-  return <div>Account page</div>;
+  const { name, email } = useAccount();
+
+  return (
+    <div className="flex flex-col gap-4">
+      <section>
+        <h2>Account</h2>
+        <p>{name}</p>
+        <p>{email}</p>
+      </section>
+
+      <section>
+        <h2>Data</h2>
+        <p>Export data to csv</p>
+        <p>Delete all data</p>
+      </section>
+
+      <section>
+        <h2>Security</h2>
+        <p>Change password</p>
+        <p>Delete account</p>
+      </section>
+    </div>
+  );
 }
 
 export default AccountPage;
