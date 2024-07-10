e779768d3f359e61c30907b67b10c21d4c7127c6
Sanjero20 <gelosantos72@gmail.com>
2024-07-11 00:37:20 +0800

    refactor: configure axios to use auth bearer token

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+  const token = Cookies.get("token");
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
