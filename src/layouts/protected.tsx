6433edaf0d11cd390dcf5c3473ca911c2b541274
Sanjero20 <gelosantos72@gmail.com>
2024-07-15 14:58:34 +0800

    fix: redirect to login page when no token present

--- src/layouts/protected.tsx
+++ src/layouts/protected.tsx
@@ -1,29 +1,41 @@
 import { useState, useEffect } from "react";
 import { useNavigate, Outlet } from "react-router-dom";
 import { useCookies } from "react-cookie";
-import { verifyAccount } from "@/services/auth.service";
-import Sidebar from "./sidebar";
 import { useAccount } from "@/stores/account";
+import { verifyAccount } from "@/services/auth.service";
+
+import Sidebar from "./sidebar";
 
 function ProtectedPage() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [, , removeCookie] = useCookies();
-  const navigate = useNavigate();
+  const [cookies, , removeCookie] = useCookies();
   const { setInfo, clearInfo } = useAccount();
+
+  const navigate = useNavigate();
+
+  const handleUnauthenticated = () => {
+    setIsAuthenticated(false);
+    removeCookie("token");
+    navigate("/login", { replace: true });
+    clearInfo();
+  };
 
   // Auth guard
   useEffect(() => {
     const verifyAccess = async () => {
-      const { isLoggedIn, name, email } = await verifyAccount();
-
-      if (isLoggedIn == false) {
-        setIsAuthenticated(false);
-        removeCookie("token");
-        navigate("/login", { replace: true });
-        clearInfo();
+      if (!cookies.token) {
+        handleUnauthenticated();
         return;
       }
 
+      const { isLoggedIn, name, email } = await verifyAccount();
+
+      if (isLoggedIn == false) {
+        handleUnauthenticated();
+        return;
+      }
+
+      // Authenticated
       setIsAuthenticated(true);
       setInfo(name, email);
     };
